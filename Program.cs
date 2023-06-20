8ddb04f06e40aa0e41270d6a6b956a1cf0d00335
Alexander Grigoriev <nomad1995@yandex.ru>
2023-06-20 23:38:48 +0300

    s

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+и возводит число A в натуральную степень B. */
+
+// Console.WriteLine("Enter the number A: ");
+// double A = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Enter the number B: ");
+// double B = Convert.ToDouble(Console.ReadLine());
+// double C = Math.Pow(A, B);
+// Console.WriteLine(C);
+
+
+/*Задача 27: Напишите программу, которая принимает на вход число 
+и выдаёт сумму цифр в числе.*/
+
+// Console.WriteLine("Enter the number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int r, sum = 0;
+// while (num > 0)
+// {      
+//     r=num%10;      
+//     sum=sum+r;      
+//     num=num/10;      
+// }      
+// Console.WriteLine(sum);
+
+/*Задача 29: Напишите программу, которая задаёт массив 
+из 8 элементов и выводит их на экран.*/
+
+// int[] arr = new int[8]; 
+// int i;  
+// Console.Write("Input 8 elements in the array: ");  
+// for(i=0; i<8; i++)  
+// {  
+//     Console.Write("{0} ",i);
+//     arr[i] = Convert.ToInt32(Console.ReadLine());  		
+// }  
+  
+//  Console.Write("Elements in array are: ");  
+// for(i=0; i<8; i++)  
+// {  
+//     Console.Write("{0}  ", arr[i]);  
+// } 
